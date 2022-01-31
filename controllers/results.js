1bfbd7ac6abd3f7955f8b09cd5d506076d14cc19
HOUPERT <yoann.houpert@yahoo.fr>
2022-01-31 16:16:51 +0100

    handle result conversion number

--- controllers/results.js
+++ controllers/results.js
@@ -1,11 +1,20 @@
 const debug = require('debug')(`linto:skill:v2:linto-skill:transcribe:controllers:getTranscriptionResult`)
 const REQUEST_ACCEPT_FORMAT = ['application/json', 'text/plain', 'text/vtt', 'text/srt']
+const STATE_RESULT_RECEIVE = 'result_received'
 
 module.exports = async function (msg, conf) {
-  try{ 
-    const url = `${conf.host}/results/${msg.payload.result_id}` 
+  try{
+    let url = `${conf.host}/results/${msg.payload.result_id}`
     let options = {
       headers : {}
+    }
+
+    if(msg.payload.convert_numbers || msg.payload.return_raw){
+      url += `?`
+      if(msg.payload.convert_numbers)
+        url +=`convert_numbers=${msg.payload.convert_numbers}&`
+      if(msg.payload.return_raw)
+        url +=`return_raw=${msg.payload.return_raw}&`
     }
 
     if(msg.payload.accept && REQUEST_ACCEPT_FORMAT.indexOf(msg.payload.accept) > -1){
@@ -17,7 +26,7 @@ module.exports = async function (msg, conf) {
     const result = await this.request.get(url, options, resultHandler)
 
     let json = {
-      state : 'result_received',
+      state : STATE_RESULT_RECEIVE,
       type : options.headers.accept
     }
     if(options.headers.accept === 'application/json'){
